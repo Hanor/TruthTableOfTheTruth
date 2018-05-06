57ec4cc7c833f2da9de99f152c600fabc255512a
Hanor Sátiro Cintra <hanor_13ctb@hotmail.com>
2018-05-06 01:46:07 -0300

    Truth table prototype was done

--- src/app/truth/truth.table/truth.table.component.ts
+++ src/app/truth/truth.table/truth.table.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { BehaviorSubject } from 'rxJs';
 
 @Component({
   selector: 'app-truth.table',
@@ -6,14 +7,83 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./truth.table.component.scss']
 })
 export class TruthTableComponent implements OnInit {
+  @ViewChild("result") divAddVariable: ElementRef;
   variables = [];
+  binaries: BehaviorSubject<Array<any>> = new BehaviorSubject(new Array());
+  result = { name: "result", binaries: this.binaries };
+  resultWasEdited = false;
+  newVariable = false;
+  editing = false;
   constructor() { }
 
   ngOnInit() {
     this.variables.push({ name: "A" });
+    this.generateBinaries();
   }
 
-  addNewVariable() {
-    this.variables.push({ name: "ok" });
+  addVariable() {
+    this.newVariable = true;
+  }
+
+  canEdit() {
+    this.editing = true
+  }
+
+  editEnd(evento, i) {
+    const value = evento.target.value;
+    if ( value && value !== "") {
+      const variable =  this.variables[i];
+      variable.name = value;
+    } else if ( this.variables.length > 1 ) {
+      this.removeVariable( i );
+    } else {
+      evento.target.value = this.variables[i].name;
+    }
+  }
+
+  generateBinaries() {
+    const size = this.variables.length;
+    const binaries = Math.pow( 2, size );
+    for( let i = 0; i < size; i++ ) {
+      const variable = this.variables[i];
+      variable.binaries = [];
+      const inversor = binaries / (Math.pow(2, (i + 1)));
+      for ( let j = 0; j < binaries; j++ ) {
+        const rest = parseInt((( j / inversor ) + ""), 10);
+        variable.binaries.push(( rest % 2 === 0 ) ? 0 : 1);
+      }
+    }
+    const values = [];
+    for( let i = 0; i < binaries; i++) {
+      values.push({indice: i, number: 1, expression: ""});
+    }
+    this.result.binaries.next(values);
+  }
+
+  haveNewVariable( value ) {
+    setTimeout(() => {
+      if ( this.newVariable && value && value !== "") {
+        this.variables.push({ name:value })
+        this.generateBinaries();
+      }
+      this.newVariable = false;
+    }, 100)
+  }
+
+  resultEdit( event, binary ) {
+    const value =  event.target.value;
+    if ( value != 0 && value != 1 ) {
+      event.target.value = binary.number
+    } else {
+      binary.number = value;  
+    }
+  }
+
+  removeVariable( i ) {
+    if ( this.variables.length > 1 ) {
+      const removed = this.variables.splice(i, 1);
+      this.generateBinaries();
+      return removed;
+    }
   }
 }

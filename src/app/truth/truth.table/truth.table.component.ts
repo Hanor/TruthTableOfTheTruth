97573d31207f6e60e15d3bb44ecffe94c756b021
Hanor Sátiro Cintra <hanor_13ctb@hotmail.com>
2018-05-11 01:49:30 -0300

    refactoring

--- src/app/truth/truth.table/truth.table.component.ts
+++ src/app/truth/truth.table/truth.table.component.ts
@@ -8,11 +8,11 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class TruthTableComponent implements OnInit {
   @ViewChild("result") divAddVariable: ElementRef;
-  binaries: BehaviorSubject<Array<any>> = new BehaviorSubject(new Array());
+  lines$: BehaviorSubject<Array<any>> = new BehaviorSubject(new Array());
   editing = false;
   finalExpression = null;
   newVariable = false;
-  result = { name: "result", binaries: this.binaries };
+  result = { name: "result", lines: this.lines$ };
   resultWasEdited = false;
   variables = [];
   constructor() { }
@@ -44,19 +44,19 @@ export class TruthTableComponent implements OnInit {
 
   generateBinaries() {
     const size = this.variables.length;
-    const binaries = Math.pow( 2, size );
+    const lines = Math.pow( 2, size );
     for( let i = 0; i < size; i++ ) {
       const variable = this.variables[i];
-      variable.binaries = [];
-      const inversor = binaries / (Math.pow(2, (i + 1)));
-      for ( let j = 0; j < binaries; j++ ) {
+      variable.lines = [];
+      const inversor = lines / (Math.pow(2, (i + 1)));
+      for ( let j = 0; j < lines; j++ ) {
         const rest = parseInt((( j / inversor ) + ""), 10);
-        variable.binaries.push(( rest % 2 === 0 ) ? 0 : 1);
+        variable.lines.push(( rest % 2 === 0 ) ? 0 : 1);
       }
     }
     const newValues = [];
-    const oldValues = this.result.binaries.getValue();
-    for( let i = 0; i < binaries; i++) {
+    const oldValues = this.result.lines.getValue();
+    for( let i = 0; i < lines; i++) {
       let oldValue = oldValues[i];
       if ( oldValue && oldValue.number == "1" ) {
         newValues.push( oldValue );
@@ -65,7 +65,7 @@ export class TruthTableComponent implements OnInit {
         newValues.push({indice: i, number: 0, expression: ""});
       }
     }
-    this.result.binaries.next(newValues);
+    this.result.lines.next(newValues);
     this.minExpression();
   }
 
@@ -82,7 +82,7 @@ export class TruthTableComponent implements OnInit {
   minExpressionForLine( line ) {
     let expression = "";
     for ( let variable of this.variables ) {
-      let argument = (variable.binaries[ line.indice ] == "1") ? variable.name : ("!" + variable.name);
+      let argument = (variable.lines[ line.indice ] == "1") ? variable.name : ("!" + variable.name);
       expression += (expression != "") ? ("." + argument)  : argument
     }
     line.expression = expression;
@@ -90,24 +90,24 @@ export class TruthTableComponent implements OnInit {
 
   minExpression() {
     this.finalExpression = "";
-    for ( let binary of this.binaries.getValue() ) {
-      if ( binary.expression ) {
-        this.finalExpression += "( "+ binary.expression +" ) + "
+    for ( let line of this.lines$.getValue() ) {
+      if ( line.expression ) {
+        this.finalExpression += "( "+ line.expression +" ) + "
       }
     }
     this.finalExpression = this.finalExpression.replace(/ \+ $/g, "");
   }
 
-  resultEdit( event, binary ) {
+  resultEdit( event, line ) {
     const value =  (event.target.value).toLowerCase();
     if ( value == "" || ( value != "0" && value != "1" && value != "x" )) {
-      event.target.value = binary.number
+      event.target.value = line.number
     } else {
-      binary.number = value;
+      line.number = value;
       if ( value == 1 ) {
-        this.minExpressionForLine( binary );
+        this.minExpressionForLine( line );
       } else {
-        binary.expression = "";
+        line.expression = "";
       }
       this.minExpression();
     }
